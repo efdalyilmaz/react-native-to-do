4e4193b6ae2ab2df60c87e97d166f8e87aae45bd
Efdal Yılmaz <efdalyilmaz@Efdal-MacBook-Pro.local>
2022-01-27 01:08:01 +0300

    efdal adds switch for all row

--- Row.js
+++ Row.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Text, View, StyleSheet} from 'react-native';
+import { Text, View, StyleSheet, Switch} from 'react-native';
 
 export default function Row(props) {
   return (
     <View style={styles.container}>
+      <Switch value={props.complete}/>
+    <View style={styles.textWrap}>
         <Text style={styles.text}>{props.text}</Text>
+    </View>
     </View>
   );
 }
@@ -16,8 +19,13 @@ const styles = StyleSheet.create({
         alignItems: "flex-start",
         justifyContent:"space-around"
     },
+    textWrap:{
+      flex:1,
+      marginHorizontal:10,
+    },
     text:{
         fontSize : 24,
         color: "#4d4d4d"
     }
+
 });
